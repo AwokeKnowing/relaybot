65ee98428ecd549e5571c6a2d067b1e1e442162b
AwokeKnowing <jamesdavidmorris@gmail.com>
2021-10-14 17:02:04 -0700

    relay

--- test.py
+++ test.py
@@ -4,15 +4,15 @@
 import RPi.GPIO as GPIO
 import time
 
-RelayTop = [21, 20, 26]
-RelayLow = [16, 19, 13]
+RelayLow = [21, 20, 26]
+RelayTop = [16, 19, 13]
 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(RelayTop, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(RelayLow, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RelayLow, GPIO.OUT, initial=GPIO.LOW)                              
 time.sleep(2)
 try:
   while True:
