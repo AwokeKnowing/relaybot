2c073fcc9eb0b5e9c40c5a05d4b265eaeba67710
AwokeKnowing <jamesdavidmorris@gmail.com>
2021-10-14 22:01:00 -0700

    relay

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ def applyRelayStates():
     GPIO.output([RelayChannels[i] 
                   for i in range(6) if RelayStates[i]==1], 
                 GPIO.HIGH)
-    time.sleep(.001)
+    time.sleep(.005)
   except:
     GPIO.cleanup()
 
