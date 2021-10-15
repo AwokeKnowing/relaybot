440a4c4c10deb1001e7afc37010daf3094a7dd04
AwokeKnowing <jamesdavidmorris@gmail.com>
2021-10-14 23:29:12 -0700

    relay

--- test.py
+++ test.py
@@ -89,31 +89,31 @@ def motors(adir, bdir):
 
 
 motors(0,0)
-time.sleep(2)
+time.sleep(1)
 
 motors(1,0)
-time.sleep(2)
+time.sleep(1)
 
 motors(-1,0)
-time.sleep(2)
+time.sleep(1)
 
 motors(0,1)
-time.sleep(2)
+time.sleep(1)
 
 motors(0,-1)
-time.sleep(2)
+time.sleep(1)
 
 motors(1,1)
-time.sleep(2)
+time.sleep(1)
 
 motors(1,-1)
-time.sleep(2)
+time.sleep(1)
 
 motors(-1,1)
-time.sleep(2)
+time.sleep(1)
 
 motors(-1,-1)
-time.sleep(2)
+time.sleep(1)
 
 motors(0,0)
 time.sleep(2)
